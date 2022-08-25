06b8925477622b2fd4c6db69f28a5917c2f573ed
Sahil-at-637888367425 <vishal.rana@hashedin.com>
2022-08-25 22:32:38 +0530

    Adding Vehicle Domain Class and configuring postgres as database

---
+++ grails-app/domain/org/example/Vehicle.groovy
@@ -0,0 +1,14 @@
+package org.example
+
+class Vehicle {
+    String name
+
+    String make
+    String model
+
+    static constraints = {
+        name maxSize: 255
+        make inList: ['Ford', 'Chevrolet', 'Nissan']
+        model nullable: true
+    }
+}
